2cfa3b4716328cfa9743c7da52b2906ed234eaeb
bondstas <sulphtc@gmail.com>
2019-05-20 13:57:40 +0300

    jason: overlay: Adapt for beast

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ $(call inherit-product, vendor/xiaomi/jason/jason-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
